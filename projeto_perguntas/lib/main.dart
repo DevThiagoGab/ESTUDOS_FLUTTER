7dece467f84a257d467ae1a043991d28f765ac54
Thiago Gabriel <thiagogabriel1904@gmail.com>
2025-01-30 00:56:06 -0300

    Adiciona novas funcionalidades
    Adiciona AppBar, botões e personalizações.

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 main() {
@@ -7,18 +5,64 @@ main() {
   runApp(new PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget{
-// @override é uma anotação usada para quando se quer modificar um método que já existe 
+class PerguntaApp extends StatelessWidget {
+// @override é uma anotação usada para quando se quer modificar um método que já existe
 // na classe mãe (superclasse).
-// Quando uma classe herda de outra, ela pode reescrever métodos da classe mãe. O @override deixa 
+// Quando uma classe herda de outra, ela pode reescrever métodos da classe mãe. O @override deixa
 // claro que esta sendo alterado um método existente.
-@override
+  @override
 // Método build que retorna Widget
-Widget build(BuildContext context){
-  // Retorna a criação de uma instância widget MaterialApp
-  return MaterialApp(
-    // Atributos nomeados, Text = widget de texto
-    home: Text('Olá flutter!'),
-  );}
-
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é seu animal favorito?',
+    ];
+    // Retorna a criação de uma instância widget MaterialApp
+    return MaterialApp(
+      // Atributos nomeados, Scaffold fornece estrutura básica para
+      // layout
+      home: Scaffold(
+        // Barra de navegação
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+          centerTitle: true, // Centraliza o título
+          titleTextStyle: const TextStyle(
+            color: Colors.white, // Cor da fonte
+            fontSize: 20, // Tamanho da fonte
+            fontWeight: FontWeight.bold, // Negrito
+          ),
+          backgroundColor: Colors.blueAccent, // Cor de fundo da appBar
+        ),
+        // Body = conteúdo principal, só aceita um widget
+        // Ctrl + . = cria a estrutura do column
+        // Column organiza widgets verticalmente
+        body: Column(
+          // children = Lista de widgets organizados verticalmente
+          children: <Widget>[
+            // Pode ser exibido das duas formas
+            Text(perguntas.elementAt(0)),
+            Text(perguntas[1]),
+            ElevatedButton(
+                onPressed: () {
+                  print('Botão pressionado!');
+                },
+                child: const Text('ElevatedButton')
+                ),
+                TextButton(
+                onPressed: () {
+                  print('Botão pressionado!');
+                },
+                child: const Text('TextButton')
+                ),
+                  OutlinedButton(
+                  onPressed: () {
+                    print('Botão pressionado!');
+                  },
+                  child: const Text('OutlinedButton')
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
 }
